abdfa8f46eb5dc8e59547a6f8ca7df5e3771f22e
Fernando <fernando.alvarez@islas.org.mx>
2020-03-30 11:43:37 -0700

    agrega find_packages al setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-import setuptools
+
+from setuptools import setup, find_packages
 
 setuptools.setup(
     name="dummy_transformations",
     version="0.1.0",
+     packages=find_packages(),
     author="Fernando Alvarez",
     python_requires='>=3.6',
 )
