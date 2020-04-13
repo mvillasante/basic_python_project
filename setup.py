088b9c2a431b77e4dd60b415c568627775963f8e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2020-04-13 11:18:55 -0700

    Elimina coma innecesaria

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 
 from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="dummy_transformations",
     version="0.1.0",
     packages=["dummy_transformations"],
     author="Fernando Alvarez",
-    python_requires='>=3.6',
+    python_requires='>=3.6'
 )
