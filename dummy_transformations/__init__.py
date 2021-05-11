fb96d2ca8eff04577daf2cb8bbcc3e32a1305bc9
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-05-11 12:10:40 -0700

    🔥🔧Change to use pyproject

--- dummy_transformations/__init__.py
+++ dummy_transformations/__init__.py
@@ -1,4 +1,4 @@
 """An amazing sample package!"""
 
-__version__ = '0.3.0'
+__version__ = "0.3.0"
 from .transformations import add_offset  # noqa
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="dummy_transformations",
-    version="0.3.0",
-    packages=["dummy_transformations"],
-    author="Ciencia de Datos • GECI",
-    python_requires=">=3.9",
-)
